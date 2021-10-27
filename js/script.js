e7f492e44ca31a2546fa684877cc75229186f55f
Vander <bram1105@hotmail.com>
2021-10-27 20:34:57 +0200

    Hamburger menu
    working on js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const hambugerMenu = document.querySelector(".hamburger");
+console.log(hambugerMenu); // NANI
+
+// Open / close hamburger menu in nav bar
+const toggleHamburger = function () {
+  console.log("toggle class");
+};
